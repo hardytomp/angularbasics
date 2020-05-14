3ed7bf3c412f86dfce9aa06854de13198e1a2f25
Hardy <hardytomp@gmail.com>
2020-05-14 13:36:08 +0530

    Data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,9 @@ import { SuccessalertComponent } from './successalert/successalert.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,14 +5,32 @@ import { Component, OnInit } from '@angular/core';
   //for giving as attribute to html default tag
  // selector:'[app-servers]',
   selector:'.app-servers',
-  template: '<app-server></app-server><app-server></app-server><app-server></app-server>',
-  styleUrls: ['./servers.component.css']
+ // template: '<app-server></app-server><app-server></app-server><app-server></app-server>',
+ templateUrl:'./servers.component.html', 
+ styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  allowNewServer = false;
+  serverCreationStatus = 'No Server was created!';
+  serverName = 'ServerName is';
+  constructor() { 
+    setTimeout( ()=> {
+      this.allowNewServer = true;
+    },2000)
+
+  }
 
   ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'server was created Name is '+this.serverName;
+  }
+
+  onUpdateServerName(event:any) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
